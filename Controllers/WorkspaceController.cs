834d1545c58864d6ea3b711fe7eae2c34ff427ac
Rizqial5 <bagusriski1@gmail.com>
2025-07-17 20:17:52 +0700

    add : add workspace view

---
+++ Controllers/WorkspaceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.App.ViewModel.Workspace;
+
+namespace ProjectManagement.App.Controllers
+{
+    public class WorkspaceController : Controller
+    {
+        [HttpPost]
+        public IActionResult GoToWorkspace(int projectId, string projectName)
+        {
+            var url = Url.Action("Index", "Workspace", new { ProjectID = projectId, ProjectName = projectName });
+            return Json(new { success = true, url });
+        }
+
+        public IActionResult Index(WorkspaceViewModel workspaceViewModel)
+        {
+            // workspaceViewModel.ProjectID dan ProjectName akan terisi dari query string
+            return View(workspaceViewModel);
+        }
+    }
+}
---
+++ ViewModel/Workspace/WorkspaceViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ProjectManagement.App.ViewModel.Workspace
+{
+    public class WorkspaceViewModel
+    {
+        public string ProjectName { get; set; }
+        public int ProjectID { get; set; }
+    }
+}
