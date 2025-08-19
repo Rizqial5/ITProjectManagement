6ad771961d0df97f34e326a08ba5576ae55c2c69
Rizqial5 <bagusriski1@gmail.com>
2025-08-19 21:57:06 +0700

    add : add task detail page

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagement.App.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpGet("/project/{projectId}/task/{taskId}")]
+        public IActionResult Details(int projectId, int taskId)
+        {
+            return View();
+        }
+    }
+}
