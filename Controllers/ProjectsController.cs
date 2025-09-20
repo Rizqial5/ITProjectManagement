25aca99df7afd5c6a6d71b06f9c8a2c99534c559
Rizqial5 <bagusriski1@gmail.com>
2025-09-20 18:05:29 +0700

    Add : implement Project Controller, page cshtml, search box

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagement.App.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Page()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/project.js
@@ -0,0 +1,4 @@
+﻿function createProject() {
+    var dialog = document.getElementById("createProjectDialog").ej2_instances[0];
+    dialog.show();
+}
