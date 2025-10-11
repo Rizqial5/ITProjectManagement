f2ad35d99f7088a478026fb685eb2b06d81610d8
Rizqial5 <bagusriski1@gmail.com>
2025-10-11 16:07:54 +0700

    add : Implement view details button on prject page

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -1,11 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.App.Repository.Interface;
+using ProjectManagement.App.ViewModel.Project;
+using System.Security.Claims;
 
 namespace ProjectManagement.App.Controllers
 {
     public class ProjectsController : Controller
     {
-        public IActionResult Page()
+        private readonly IProjectRepository _projectRepository;
+
+        public ProjectsController(IProjectRepository projectRepository)
         {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<IActionResult> Page()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var allProject = await _projectRepository.GetAllAsync(userId);
+
+            var data = allProject.Select(i => new ProjectCardViewModel()
+            {
+                Id = i.Id,
+                Title = i.Name,
+                Description = i.Description,
+                Status = i.Tasks.Count != 0 ? i.Tasks.FirstOrDefault().Status : Models.Enum.Status.ToDo,
+                TotalTasks = i.Tasks.Count,
+                CompletedTasks = i.Tasks.Count != 0 ? i.Tasks.Where(i => i.Status == Models.Enum.Status.Done).Count() : 0,
+                DueDate = DateTime.MaxValue,
+                IsConnected = false
+            });
+
+
+
+            return View(data);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            //var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            //var allProject = await _projectRepository.GetAllAsync(userId);
+
+            //var data = allProject.Select(i => new ProjectCardViewModel()
+            //{
+            //    Id = i.Id,
+            //    Title = i.Name,
+            //    Description = i.Description,
+            //    Status = i.Tasks.Count != 0 ? i.Tasks.FirstOrDefault().Status : Models.Enum.Status.ToDo,
+            //    TotalTasks = i.Tasks.Count,
+            //    CompletedTasks = i.Tasks.Count != 0 ? i.Tasks.Where(i => i.Status == Models.Enum.Status.Done).Count() : 0,
+            //    DueDate = DateTime.MaxValue,
+            //    IsConnected = false
+            //});
+
+
+
             return View();
         }
     }
---
+++ ViewModel/Project/ProjectCardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ProjectManagement.App.Models.Enum;
+
+namespace ProjectManagement.App.ViewModel.Project
+{
+    public class ProjectCardViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Status Status { get; set; }
+        public int CompletedTasks { get; set; }
+        public int TotalTasks { get; set; }
+        public float Progress => TotalTasks != 0 ? ((float)CompletedTasks / TotalTasks) * 100 : 0;
+        public DateTime DueDate { get; set; }
+        public bool IsConnected { get; set; }
+        //public List<MemberViewModel> Members { get; set; } = new();
+    }
+}
